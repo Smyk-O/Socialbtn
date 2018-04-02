b53ab8c9e66125c4aef5033ff23938d39fd604d1
Smyk <smyk006@yandex.ru>
2018-04-02 20:52:07 +0300

    step 2

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 $(document).ready(function () {
+    var itembox = $('#box');
     $('#buttonscollapsed').click(function () {
         $(this).toggleClass('buttonclose');
-        $('#box').toggleClass('box')
-        $('.item').toggle(500)
-        if (!$('.box').hasClass('box')) {
-            $('#box').animate({
-                'width': '60px'
-            }, 500);
+        itembox.toggleClass('box')
+        if (!$('#tumbler').hasClass('tumbler')) {
+            if (!itembox.hasClass('box')) {
+                close();
+            } else {
+                itembox.animate({
+                    'width': '322px',
+                    'height': '60px'
+                }, 500);
+                var rIndent = 22;
+                $('.item').each(function () {
+                    rIndent += 52
+                    $(this).css({
+                        'right': + rIndent + 'px',
+                        'bottom': '20px'
+                    });
+                });
+            }
         } else {
-            $('#box').animate({
-                'width': '322px'
-            }, 500);
+            if (!itembox.hasClass('box')) {
+                close();
+            } else {
+                itembox.animate({
+                    'height': '322px',
+                    'width': '60px'
+                }, 500);
+                var rIndent = 22;
+                $('.item').each(function () {
+                    rIndent += 52
+                    $(this).css({
+                        'right': '20px',
+                        'bottom': + rIndent + 'px',
+                    });
+                });
+            }
+        }
+        $('.item').toggle(500)
+    });
+
+    $('#tumbler').click(function () {
+        $('#tumbler').toggleClass('tumbler')
+        if ($('#tumbler').hasClass('tumbler')) {
+            $('#tumbler').css({
+                'transform': 'rotate(90deg)'
+            });
+        } else {
+            $('#tumbler').css({
+                'transform': 'rotate(0deg)'
+            });
         }
     });
+
+    function close() {
+        itembox.animate({
+            'width': '60px',
+            'height': '60px'
+        }, 500);
+    };
 });
